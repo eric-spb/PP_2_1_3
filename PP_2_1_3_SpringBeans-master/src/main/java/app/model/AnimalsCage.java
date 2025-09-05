6cd6bf9f1cb0b79ce78388e6e632d3355780bf32
eric-spb <er.fv@icloud.com>
2025-09-05 11:33:30 +0300

    Add files via upload

---
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,34 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Component;
+import app.model.Timer;
+
+@Component
+public class AnimalsCage {
+
+
+    private Animal animal;
+    private Timer timer;
+    //dog
+    //cat
+
+    @Autowired
+    public AnimalsCage(@Qualifier("cat") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+}
---
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/Dog.java
@@ -0,0 +1,9 @@
+package app.model;
+
+import org.springframework.stereotype.*;
+
+@Component
+public class Dog extends Animal {
+    @Override
+    public String toString() {return "Im Dog";};
+}
